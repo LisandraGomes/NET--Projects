dc42404a20b03104ec84d1cb7e4c0fa35c67aec8
Lisandra <lisandra.gomes@topdown.com.br>
2022-04-03 16:35:21 -0300

    Primeiros projetinhos do zero, relembrando c#

---
+++ Calculadora/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Calculadora{
+
+    class Program 
+    { 
+        static void Main(String[] args)
+        {
+            inicie:
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Operação:");
+            string operacao = Console.ReadLine();
+
+            Console.WriteLine("Segundo Valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+
+          switch(operacao){
+              case "+": Soma(v1,v2); break;
+              case "-": Sub(v1,v2); break;
+              case "*": Mult(v1,v2); break;
+              case "0": break;
+          }
+            string cont = Console.ReadLine();
+            if(cont.Equals(string.Empty))
+                goto inicie;
+        }
+        static void Mult(float valor1, float valor2){
+
+            Console.WriteLine("");
+
+            float resultado = valor1*valor2;
+            
+            Console.WriteLine($"Resultado: {resultado}");
+
+        }
+        static void Sub(float valor1, float valor2){
+
+            Console.WriteLine("");
+
+            float resultado = valor1-valor2;
+            
+            Console.WriteLine($"Resultado: {resultado}");
+
+        }
+        static void Soma(float valor1, float valor2){
+
+            Console.WriteLine("");
+
+            float resultado = valor1+valor2;
+            
+            Console.WriteLine($"Resultado: {resultado}");
+
+        }
+
+    }
+}
---
+++ Conometro/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Calculadora{
+
+    class Program 
+    { 
+        static void Main(String[] args)
+        {
+            start(15);
+        }
+
+        static void Menu(){
+
+            Console.WriteLine("Insira q quantidade de tempo desejada: (S - Segundos M - Minutos");
+            string tempo = Console.ReadLine().ToLower();
+
+        }
+
+        static void start(int time){
+            int currentTime = 0;
+
+            while( currentTime != time ){
+                currentTime++;
+                string a="0";
+                Console.Clear();
+                if(currentTime>=10)
+                    a = "";
+                Console.WriteLine($"00:00:{a}{currentTime}");
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("Tempo Finalizado");
+
+            Thread.Sleep(5000);
+        }
+    }
+}
