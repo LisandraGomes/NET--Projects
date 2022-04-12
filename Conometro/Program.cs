db9f60afd859a67049bdf0d9171f430e63e26e6a
LisandraGomes <lisandragomes53@gmail.com>
2022-04-12 15:26:54 -0300

    Relembrando, c#, pequenos exercícios

--- Conometro/Program.cs
+++ Conometro/Program.cs
@@ -1,35 +1,66 @@
-﻿namespace Calculadora{
+﻿using System;
+using System.Threading;
 
-    class Program 
-    { 
-        static void Main(String[] args)
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
         {
-            start(15);
+            Menu();
         }
 
-        static void Menu(){
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
 
-            Console.WriteLine("Insira q quantidade de tempo desejada: (S - Segundos M - Minutos");
-            string tempo = Console.ReadLine().ToLower();
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
 
+            if (type == 'm')
+                multiplier = 60;
+
+            if (time == 0)
+                System.Environment.Exit(0);
+
+            PreStart(time * multiplier);
         }
 
-        static void start(int time){
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(2500);
+
+            Start(time);
+        }
+
+        static void Start(int time)
+        {
             int currentTime = 0;
 
-            while( currentTime != time ){
-                currentTime++;
-                string a="0";
+            while (currentTime != time)
+            {
                 Console.Clear();
-                if(currentTime>=10)
-                    a = "";
-                Console.WriteLine($"00:00:{a}{currentTime}");
+                currentTime++;
+                Console.WriteLine(currentTime);
                 Thread.Sleep(1000);
             }
 
-            Console.WriteLine("Tempo Finalizado");
-
-            Thread.Sleep(5000);
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado");
+            Thread.Sleep(2500);
+            Menu();
         }
     }
 }
---
+++ EditorTexto/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace TextEditor
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.Write("Menu:\n 1 - Abrir Arquivo\n 2 - Criar Novo Arquivo\n 0 - Sair\n\n ----Arquivos TXT-----\n R:");
+            short opcao = short.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 0: System.Environment.Exit(0); break;
+                case 1: abrirArquivo(); break;
+                case 2: editarArquivo(); break;
+                default: Menu(); break;
+            }
+        }
+
+        static void abrirArquivo()
+        {
+            Console.Clear();
+            Console.Write("Nome do arquivo:");
+            string caminho = "D:\\.NET\\NET--Projects\\EditorTexto\\Arquivo\\" + Console.ReadLine() + ".txt";
+            Console.Clear();
+            Console.WriteLine("Para voltar ao Menu aperte enter...");
+            Thread.Sleep(2000);
+            Console.Clear();
+            using (var arquivo = new StreamReader(caminho))
+            {
+                string texto = arquivo.ReadToEnd();
+                Console.WriteLine(texto);
+            }
+            if ((Console.ReadKey().Key == ConsoleKey.Enter))
+            {
+                Menu();
+            }
+        }
+
+        static void editarArquivo()
+        {
+            edicao();
+            string texto = "";
+
+            do
+            {
+                texto += Console.ReadLine();
+                texto += Environment.NewLine;
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+            SalvarArquivo(texto);
+
+            edicao();
+
+            Thread.Sleep(500);
+
+            Menu();
+        }
+        /*D:\.NET\NET--Projects\EditorTexto\Arquivo\Arquivooi.txt*/
+
+        static void SalvarArquivo(string texto)
+        {
+            Console.Clear();
+            Console.Write("Insira nome para salvar o arquivo:");
+            var caminho = "D:\\.NET\\NET--Projects\\EditorTexto\\Arquivo\\" + Console.ReadLine() + ".txt";
+
+            using (var arquivo = new StreamWriter(caminho))
+            {
+                arquivo.Write(texto);
+            }
+            Console.WriteLine($"Arquivo Salvo, caminho:{caminho}");
+        }
+
+        static void edicao()
+        {
+            Console.Clear();
+            Console.Write("Aguarde.");
+            Thread.Sleep(850);
+            Console.Write(".");
+            Thread.Sleep(850);
+            Console.Write(".");
+            Thread.Sleep(850);
+            Console.Write(".");
+            Console.Clear();
+        }
+
+    }
+}
