b7f65bff68d7165c3b9197e664999d03b26a61dd
LisandraGomes <lisandragomes53@gmail.com>
2022-11-18 18:38:51 -0300

    Iniciando API de pessoa em .NET 6 Fiz a entidade e a validação genérica dos campos no Domain.

---
+++ 5.API/APIPessoa/Api.Domain/Entidades/Pessoa.cs
@@ -0,0 +1,55 @@
+﻿using Api.Domain.Validations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Domain.Entidades
+{
+    public sealed class Pessoa
+    {
+        public Guid Id { get; private set; }
+        public string Usuario { get; set; }
+        public string Nome { get; private set; }
+        public string Sobrenome { get; private set; }
+        public DateTime DataNascimento { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; private set; }
+
+        public Pessoa(string Nome, string Sobrenome, string Telefone, DateTime DataNascimento, string Email, string Senha)
+        {
+            Validacao(Nome, Sobrenome, Telefone, DataNascimento, Email, Senha);
+        }
+
+        //METODO PARA EDITAR A PESSOA
+        public Pessoa(int id,string Usuario, string Nome, string Sobrenome, string Telefone, DateTime DataNascimento, string Email, string Senha) 
+        {
+            DomainValidationException.When(id < 0, "Id de Usuario Inválido!");
+            DomainValidationException.When(string.IsNullOrEmpty(Usuario), "Usuário é Invalido");
+            DomainValidationException.When(Usuario.Equals("1234"), "Usuário não pode ser alterado!");
+
+            Validacao(Nome, Sobrenome, Telefone, DataNascimento, Email, Senha);
+        }
+
+        //Validação Generica de Entidades
+        private void Validacao(string Nome, string Sobrenome, string Telefone, DateTime DataNascimento, string Email, string Senha)
+        {
+            DomainValidationException.When(string.IsNullOrEmpty(Nome), "Nome deve ser informado!");
+            DomainValidationException.When(string.IsNullOrEmpty(Sobrenome), "Sobrenome deve ser informado");
+            DomainValidationException.When(string.IsNullOrEmpty(Telefone), "Telefone deve ser informado");
+            DateTime data = DataNascimento;
+            DomainValidationException.When(data.Equals(null), "Data deve ser informada!");
+            DomainValidationException.When(string.IsNullOrEmpty(Email), "Email deve ser informado!");
+            DomainValidationException.When(string.IsNullOrEmpty(Senha), "Senha deve ser informada!");
+
+            Nome = Nome;
+            Sobrenome = Sobrenome;
+            Telefone = Telefone;
+            DataNascimento = DataNascimento;
+            Email = Email;  
+            Senha = Senha;
+        }
+    }
+}
---
+++ 5.API/APIPessoa/Api.Domain/Validations/DomainValidationException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Domain.Validations
+{
+    public class DomainValidationException : Exception
+    {
+        public DomainValidationException(string error) : base(error) 
+        { 
+            
+        }
+        public static void When(bool temError, string mensagem)
+        {
+            if (temError)
+                throw new DomainValidationException(mensagem);
+        }
+    }
+}
