2171d5f5e5afe641fdd91d531150651eb5020cc1
LisandraGomes <lisandragomes53@gmail.com>
2022-12-14 22:21:13 -0300

    Continuação do mão na massa de FOO

--- 6.Balta.io - Fundamentos Orientação a Objetos/Balta/Program.cs
+++ 6.Balta.io - Fundamentos Orientação a Objetos/Balta/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Balta.ContentContext;
+
+namespace Balta{
+    class Program{
+    static void Main(string[] args){
+        var Curso = new Curso();
+
+    }
+    }
+}
