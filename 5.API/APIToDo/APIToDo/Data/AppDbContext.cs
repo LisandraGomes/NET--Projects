ded06360dcb226296f07cdb2220c5581f14f3d48
LisandraGomes <lisandragomes53@gmail.com>
2023-01-24 21:23:06 -0300

    Adicionando API Todo, fazendo junto com o curso Fundamentos do ASP.NET6 para relembrar e fixar.

---
+++ 5.API/APIToDo/APIToDo/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using APIToDo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIToDo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<TodoModel> Todos { get; set; }
+
+        //esse banco ainda não existe, como é SQLlite, será gerado junto com os migrations.
+        protected override void OnConfiguring(DbContextOptionsBuilder options) 
+            => options.UseSqlite("DataSource=app.db; Cache=Shared");
+    }
+}
---
+++ 5.API/APIToDo/APIToDo/Models/TodoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace APIToDo.Models
+{
+    public class TodoModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool Done { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
